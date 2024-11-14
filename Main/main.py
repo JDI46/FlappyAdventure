cf6f4c8d07353400b5240965a910b13036d58e22
Xander <xwhitby20@gmail.com>
2024-11-14 12:59:44 -0700

    added movement files for character and enemies, also changed display

--- Main/main.py
+++ Main/main.py
@@ -1,9 +1,21 @@
 import pygame
 
+
 def run_game():
     pygame.init()
-    home_screen = pygame.display.set_mode((1280, 720))
+    home_screen = pygame.display.set_mode((1060, 600))
     run_game = True
+
+    background = True
+    surface = True
+
+    font = True
+    text = True
+    get_text_pos = True
+
+    
+
+
 
     while run_game:
         for event in pygame.event.get():
