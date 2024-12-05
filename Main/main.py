bd4c0ccad3b71d5668dc60cb19df4351c2370fbb
Xander <xwhitby20@gmail.com>
2024-12-04 17:34:48 -0700

    added needed functions

--- Main/main.py
+++ Main/main.py
@@ -1,2 +1,15 @@
-import django
+import pygame
 
+def metric_variable():
+    screen = 
+
+def player_movement():
+    return
+
+def menu():
+    return
+
+def play_game():
+    pygame.init()
+
+    
