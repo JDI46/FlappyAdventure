2a55d015f1a86e2b79469283b5c43d323fb67efe
Xander <xwhitby20@gmail.com>
2024-12-04 17:23:18 -0700

    just making the flappy bird game

--- Main/main.py
+++ Main/main.py
@@ -1,21 +1,2 @@
-try:
-    import sys
-    import random
-    import math
-    import os
-    import getopt
-    import pygame
-    from socket import *
-    from pygame.locals import *
-except ImportError as err:
-    print(f"couldn't load module, {err}")
-    sys.exit(2)
+import django
 
-def start_game():
-    pygame.init()
-    
-
-def load_background():
-    pass
-def load_main_character():
-    pass
