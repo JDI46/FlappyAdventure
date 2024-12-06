13086185072298af096e265d6708475c0b9b3aff
Xander <xwhitby20@gmail.com>
2024-12-06 13:19:12 -0700

    reverted back to old commit to pull up game screen

--- Main/main.py
+++ Main/main.py
@@ -1,15 +1,27 @@
 import pygame
 
-def metric_variable():
-    screen = 
-
-def player_movement():
-    return
-
-def menu():
-    return
-
-def play_game():
+def run_game():
     pygame.init()
+    home_screen = pygame.display.set_mode((1280, 720))
+    home_screen = pygame.display.set_mode((1060, 600))
+    run_game = True
 
+    background = True
+    surface = True
+    font = True
+    text = True
+    get_text_pos = True
     
+    while run_game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        home_screen.fill("blue")
+
+
+        pygame.display.flip()
+
+
+    pygame.quit()
